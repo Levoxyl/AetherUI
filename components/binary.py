423bffe6f556ddaa22d6b208cddcca5d8cc40373
Levoxyl <hereuser47@gmail.com>
2026-06-04 22:14:20 +0300

    Minimum value increase for a gap close

--- components/binary.py
+++ components/binary.py
@@ -20,19 +20,19 @@ class BinaryComponent:
         self.update()
 
     def generate_line(self):
-        return ''.join(random.choice('01') for _ in range(12))
+        return ''.join(random.choice('01') for _ in range(30))
 
     def update(self):
         self.text.config(state=tk.NORMAL)
         self.text.insert('1.0', self.generate_line() + '\n')
 
         line_count = int(self.text.index('end-1c').split('.')[0])
-        if line_count > 20:
-            self.text.delete('20.0', 'end')
+        if line_count > 30:
+            self.text.delete('30.0', 'end')
         
         # red flash
         if random.random() < 0.05:
-            line_num = random.randint(1, min(20, line_count))
+            line_num = random.randint(1, min(30, line_count))
             tag_name = f'err_{random.randint(1,1000)}'
             self.text.tag_add(tag_name, f'{line_num}.0', f'{line_num}.end')
             self.text.tag_config(tag_name, foreground='red')
